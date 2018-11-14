e3c29549f43649401ae04f803a8cb50499980610
Peter Oas <oas.peter@gmail.com>
2018-11-14 15:06:01 -0500

    halfway thru chapter 5

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    number = 5
+    return render(request, 'index.html', {
+        'number': number,
+    })
