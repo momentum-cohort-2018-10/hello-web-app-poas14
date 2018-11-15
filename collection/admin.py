df2781ef4cbf7261a4e7d16526829895e658fc16
Peter Oas <oas.peter@gmail.com>
2018-11-14 20:18:09 -0500

    finished chapter six, working version with object 'rocket'

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from collection.models import Rocket
+
+class RocketAdmin(admin.ModelAdmin):
+    model = Rocket
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
 # Register your models here.
+admin.site.register(Rocket, RocketAdmin)
